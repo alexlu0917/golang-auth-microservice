a09fac9d946ead044e05c479c9a67bd5e2146733
alexlu0917 <alexlu0917.dev@gmail.com>
2023-07-03 08:37:01 -0500

    create CredentialService interface

--- domain/interfaces.go
+++ domain/interfaces.go
@@ -6,4 +6,11 @@ import (
 
 type AuthService interface {
 	Authenticate(context.Context, AuthParams) (AuthTokenID, error)
+	Validate(context.Context, AuthTokenID) error
+	Expire(context.Context, AuthTokenID) error
+}
+
+type CredentialService interface {
+	SaveCredential(context.Context, SaveParams) error
+	ListCredentials(context.Context) ([]Credential, error)
 }
--- domain/types.go
+++ domain/types.go
@@ -1,8 +1,28 @@
 package domain
 
+import "time"
+
 type AuthTokenID string
+type CredentialID uint
+type AuthToken struct {
+	ID        AuthTokenID
+	ExpiresAt time.Time
+}
 
 type AuthParams struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
+}
+
+type SaveParams struct {
+	ID       CredentialID `json:"id"`
+	Name     string       `json:"name"`
+	Password string       `json:"password"`
+}
+
+type Credential struct {
+	ID           CredentialID `json:"id"`
+	Name         string       `json:"name"`
+	PasswordHash string       `json:"-"`
+	AuthToken    AuthToken    `json:"-"`
 }
